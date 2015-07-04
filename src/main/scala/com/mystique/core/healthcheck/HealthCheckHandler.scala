6d63246ad1ce16e968fb6128b3d9e85a167b6402
Leon Maia <hi@leonmaia.com>
2015-07-04 13:53:22 -0300

    trace on healthcheck

--- src/main/scala/com/mystique/core/healthcheck/HealthCheckHandler.scala
+++ src/main/scala/com/mystique/core/healthcheck/HealthCheckHandler.scala
@@ -1,14 +1,17 @@
 package com.mystique.core.healthcheck
 
 import com.mystique.server.http.Responses._
+import com.mystique.service.tracing.Tracing
 import com.twitter.finagle.Service
 import com.twitter.finagle.http.{Request, Response}
 import com.twitter.util.Future
 import org.jboss.netty.handler.codec.http.HttpResponseStatus
 
-class HealthCheckHandler() extends Service[Request, Response] {
+class HealthCheckHandler() extends Service[Request, Response] with Tracing {
 
   def apply(request: Request): Future[Response] =  {
-    Future(respond("It Works! Yay!", HttpResponseStatus.OK))
+    withTrace("Healthcheck - #apply", "HealthCheck") {
+      Future(respond("It Works! Yay!", HttpResponseStatus.OK))
+    }
   }
 }
---
+++ src/main/scala/com/mystique/service/tracing/Tracing.scala
@@ -0,0 +1,36 @@
+package com.mystique.service.tracing
+
+import com.twitter.finagle.RequestTimeoutException
+import com.twitter.finagle.tracing.{Annotation, Trace}
+import com.twitter.finagle.util.DefaultTimer
+import com.twitter.logging.Logger
+import com.twitter.util.Duration._
+import com.twitter.util.{Duration, Future}
+
+trait Tracing {
+
+  val log = Logger(getClass)
+
+  def withTrace[T](id: String, protocol: String = "custom", timeout: Duration = fromSeconds(1)) (block: => Future[T]) = {
+    Trace.traceService(id, protocol, Option.empty) {
+      Trace.record(Annotation.ClientSend())
+      val time = System.currentTimeMillis()
+      withTimeout(id, timeout, block) map { res =>
+        log.info(s"$id - Mystique API took ${System.currentTimeMillis() - time}ms to respond")
+        Trace.record(Annotation.ClientRecv())
+        res
+      }
+    }
+  }
+
+  private[tracing] def withTimeout[T](id: String = s"${getClass.getSimpleName}", duration: Duration, block: => Future[T]): Future[T] = {
+    block.within(
+    DefaultTimer.twitter,
+    duration, {
+      Trace.record(s"$id.timeout")
+      new RequestTimeoutException(duration, s"Timeout exceed while accessing using $id")
+    }
+    )
+  }
+}
+
