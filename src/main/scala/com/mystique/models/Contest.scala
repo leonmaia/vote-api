dbce80c8d0690c63ef910bd94279ef29e0fe8589
Leon Maia <hi@leonmaia.com>
2015-07-04 15:25:01 -0300

    create contest validations

--- src/main/scala/com/mystique/models/Contest.scala
+++ src/main/scala/com/mystique/models/Contest.scala
@@ -1,9 +1,27 @@
 package com.mystique.models
 
+import com.mystique.util.DateSupport._
+
 case class Contest(slug: String,
                    name: String,
                    description: Option[String],
                    startDate: String,
-                   endDate: String ) {}
+                   endDate: String ) {
+  
+  require(slug.nonEmpty, "slug is required")
+  require(slug.length > 2 && slug.length < 21, "slug is maximum 20 and minimum 3")
 
+  require(name.nonEmpty, "name is required")
+  require(name.length > 2 && name.length < 11, "name is maximum 10 and minimum 3")
+
+  require(description.getOrElse("").length < 256, "description should be maximum 255")
+
+  require(startDate.nonEmpty, "start_date is required")
+  require(isValidDate(startDate), "start_date should be a date")
+
+  require(endDate.nonEmpty, "end_date is required")
+  require(isValidDate(endDate), "end_date should be a date")
+
+  require(fmt.parse(startDate).before(fmt.parse(endDate)), "start_date should be before end_date")
+}
 
