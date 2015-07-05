82163e5238968981a9edd76a78c9e818ae24bf22
Leon Maia <hi@leonmaia.com>
2015-07-04 23:22:46 -0300

    redisUtils refactor

--- src/main/scala/com/mystique/util/RedisUtils.scala
+++ src/main/scala/com/mystique/util/RedisUtils.scala
@@ -2,13 +2,11 @@ package com.mystique.util
 
 import com.twitter.finagle.redis.Client
 import com.twitter.finagle.redis.util.{CBToString, StringToChannelBuffer}
-import com.twitter.util.{Await, Future}
-
-import scala.collection.mutable.ListBuffer
+import com.twitter.util.Future
 
 object RedisUtils {
  implicit class SimpleRedis(val client: Client) {
-   def simpleGet(key: String) = {
+   def simpleGet(key: String): Future[Option[String]] = {
      val k = StringToChannelBuffer(key)
      client.get(k) flatMap {
        case Some(v) => Future(Some(CBToString(v)))
@@ -16,7 +14,7 @@ object RedisUtils {
      }
    }
 
-   def simpleSet(key: String, value: String) = {
+   def simpleSet(key: String, value: String): Future[Unit] = {
      val k = StringToChannelBuffer(key)
      val v = StringToChannelBuffer(value)
      client.set(k, v)
@@ -27,23 +25,12 @@ object RedisUtils {
      client.incr(k)
    }
 
-   def simpleHMSet(key: String, hm: Map[String, String]) = {
+   def simpleHMSet(key: String, hm: Map[String, String]): Future[Unit] = {
      val k = StringToChannelBuffer(key)
      val v = hm.map(r => StringToChannelBuffer(r._1) -> StringToChannelBuffer(r._2))
      client.hMSet(k, v)
    }
 
-   def simpleHMGet(key: String) = {
-     val k = StringToChannelBuffer(key)
-     client.hGetAll(k) map (
-       b => b map(cb => CBToString(cb._1) -> CBToString(cb._2))) map (r => r.toMap)
-   }
-
-   def simpleHMGetAll(keys: Seq[String]) = {
-     val arr = new ListBuffer[Map[String, String]]
-     keys.map(r => Await.result(simpleHMGet(r))) map(arr += _)
-     Future(Option(arr))
-   }
    def simpleGetKeys(pattern: String): Future[Seq[String]] = {
      val k = StringToChannelBuffer(pattern)
      client.keys(k) map (b => b map (CBToString(_)))
